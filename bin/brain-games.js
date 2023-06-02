726e2eeba8b56a830ee0efcf2311239a86f3630c
Lazy-Donut <lazy.donut@yandex.ru>
2023-06-02 11:15:22 +0600

    Step 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line import/extensions
-import { brainGamesBegin } from '../src/cli.js';
+import brainGamesBegin from '../src/cli.js';
 
-brainGamesBegin;
+brainGamesBegin();
--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const brainGamesBegin = () => {
+export default function brainGamesBegin() {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-};
+}
 
 brainGamesBegin();
